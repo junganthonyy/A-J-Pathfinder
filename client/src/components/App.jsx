78254f537f612f496fd02647b16f1bf3ddb09796
Anthony <junganthonyy@gmail.com>
2020-03-07 11:48:48 -0800

    Front End Setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+// class App extends React.Component {
+//   constructor(props){
+//     super (props);
+//   }
+
+//   render() {
+//     return (
+//       <div> this is our app! </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+export default function App(){
+  return (
+    <div>This is our app!</div>
+  )
+};
