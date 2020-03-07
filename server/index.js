901323fb0316fa76d41ec79702c4b92e2c26ad73
Anthony <junganthonyy@gmail.com>
2020-03-07 12:29:07 -0800

    Fix Cors Typo

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-const coors = require('coors');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const port = 3001;
 
 app.use(morgan('dev'));
-app.use(coors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded());
